0e045d37a3a499c376b0fa2d02be71f4105d960d
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-01 22:16:52 +0200

    type of object and optional Types

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ var user = {
   age: 18,
   isMarried: false,
   hubbies: ["Playing Soccer", "Watching Movies"],
+  gender: "Male",
+};
+var userTwo = {
+  name: "johnDoe",
+  age: 18,
+  isMarried: false,
+  hubbies: ["Playing Soccer", "Watching Movies"],
 };
 var users = [user];
--- app.ts
+++ app.ts
@@ -1,18 +1,24 @@
-const user: {
+type user = {
   name: string;
   age: number;
   isMarried: boolean;
   hubbies: string[];
-} = {
+  gender?: string;
+};
+
+const user: user = {
+  name: "johnDoe",
+  age: 18,
+  isMarried: false,
+  hubbies: ["Playing Soccer", "Watching Movies"],
+  gender: "Male",
+};
+
+const userTwo: user = {
   name: "johnDoe",
   age: 18,
   isMarried: false,
   hubbies: ["Playing Soccer", "Watching Movies"],
 };
 
-const users: {
-  name: string;
-  age: number;
-  isMarried: boolean;
-  hubbies: string[];
-}[] = [user];
+const users: user[] = [user];
