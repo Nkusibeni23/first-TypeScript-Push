55027ca3877414a1f6312fd5782f4f6b7d5ecfec
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-01 22:07:06 +0200

    app.ts

---
+++ app.js
@@ -0,0 +1,7 @@
+var user = {
+  name: "johnDoe",
+  age: 18,
+  isMarried: false,
+  hubbies: ["Playing Soccer", "Watching Movies"],
+};
+var users = [user];
---
+++ app.ts
@@ -0,0 +1,18 @@
+const user: {
+  name: string;
+  age: number;
+  isMarried: boolean;
+  hubbies: string[];
+} = {
+  name: "johnDoe",
+  age: 18,
+  isMarried: false,
+  hubbies: ["Playing Soccer", "Watching Movies"],
+};
+
+const users: {
+  name: string;
+  age: number;
+  isMarried: boolean;
+  hubbies: string[];
+}[] = [user];
